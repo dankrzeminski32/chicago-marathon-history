a291cc2f70f7312e79be890d45e65e2205c53325
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-10-25 18:30:46 -0500

    docker setup, and working marathon event objects

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
 FROM python:3.9
 
-RUN groupadd -g 999 appuser && \
-    useradd -r -u 999 -g appuser appuser
-USER appuser
-
-
 WORKDIR /home/Chicago_Marathon_History
 
 ADD ./src/scrapers/history.py .
 
-COPY requirements.txt /tmp/requirements.txt
-
-
-RUN python3 -m pip install -r /tmp/requirements.txt
+ADD requirements.txt /tmp/requirements.txt
+RUN python -m pip install -r /tmp/requirements.txt
 
 CMD python history.py
--- src/scrapers/history.py
+++ src/scrapers/history.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from bs4 import BeautifulSoup
 import requests
 
@@ -9,8 +10,7 @@ import requests
 #         print(name.text)
 
 class HistoryScraper:
-    """Scrapes Athlete data and finish times over 40 years"""
-    # UURL = "https://chicago-history.r.mikatiming.com/2019/?page=1&event=MAR_999999107FA31100000000C9&lang=EN_CAP&num_results=100&pid=list&search%5Bsex%5D=M&search%5Bage_class%5D=%25"
+    """Scrapes Athlete data and finish times over 20 years"""
     URL = "https://chicago-history.r.mikatiming.com/2015/?page=1&event=MAR_999999107FA31100000000C9&lang=EN_CAP&num_results=100&pid=list&search%5Bsex%5D=M&search%5Bage_class%5D=%25"
     content = requests.get(URL).content
     soup = BeautifulSoup(content, 'html.parser') # If this line causes an error, run 'pip install html5lib' or install html5lib
@@ -19,13 +19,46 @@ class HistoryScraper:
         race_info = self.soup.find('li', attrs={'class': 'list-group-item'})
         return race_info
 
+@dataclass
+class MarathonEvent:
+    year: int
+    id: str
+
 class EventJsonParser:
     """Parses json request to get unique event ids for history scraper"""
     URL = "https://chicago-history.r.mikatiming.com/2011/index.php?content=ajax2&func=getSearchFields&options[b][lists][event_main_group]=2015&options[b][lists][event]=&options[b][lists][sex]=&options[lang]=EN_CAP&options[pid]=start"
     content = requests.get(URL).json()
-    event_data: list[int] = content['branches']['search']['fields']['event']['data'][0]
 
-    def getJson(self):
-        return self.content['branches']['search']['fields']['event']['data'][0]
+    def get_event_list(self) -> list[dict]:
+        """returns raw list of events in json format
 
-print(EventJsonParser().getJson())
+        Returns:
+            list[dict]: Multiple dictionaries with year and various event (marathon,wheelchair, handcycle) info,
+        """
+        return self.content['branches']['search']['fields']['event']['data']
+
+    def get_marathon_data(self) -> list[dict]:
+        """returns raw list of marathon id's and marathon year information
+
+        Returns:
+            list[dict]: multiple dictionaries containing year and marathon id information
+        """
+        all_events = self.get_event_list()
+        marathon_list = [v for i, v in enumerate(all_events) if i % 4 < 2]            
+        return marathon_list
+
+    def get_parsed_marathon_events(self) -> list[MarathonEvent]:
+        """parses json data and returns a user friendly list of MarathonEvent objects
+
+        Returns:
+            list[MarathonEvent]: list of MarathonEvent objects
+        """
+        marathon_list = self.get_marathon_data()
+        parsed_marathon_events: list[MarathonEvent] = []
+        for idx in range(0, len(marathon_list)-1):
+            if idx % 2 != 0:
+                continue #skips current iteration
+            year = marathon_list[idx]['v'][0][-4:]
+            marathon_id = marathon_list[idx+1]['v'][0]
+            parsed_marathon_events.append(MarathonEvent(year, marathon_id))
+        return parsed_marathon_events
