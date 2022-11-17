232e6bdd7f11888d315f2e3052b86507267c7c41
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-11-16 18:34:40 -0600

    finished bar chart

--- src/frontend/src/components/graphs/BarMaleFemaleParticipants.js
+++ src/frontend/src/components/graphs/BarMaleFemaleParticipants.js
@@ -5,23 +5,41 @@ import { Chart, registerables } from "chart.js";
 
 export const BarMaleFemaleParticipants = ({ marathon }) => {
     const [chartData, SetChartData] = useState({});
+    const [chartOptions, SetChartOptions] = useState({});
     const [haveData, setHaveData] = useState(false);
 
     Chart.register(...registerables);
     console.log(marathon);
+
     useEffect(() => {
         SetChartData({
             labels: ["Males", "Females"],
             datasets: [
                 {
-                    label: "Number of Participants",
                     data: [
                         marathon.num_athletes_male,
                         marathon.num_athletes_female,
                     ],
-                    backgroundColor: ["#ffbb11", "#ecf0f1"],
+                    backgroundColor: ["skyblue", "pink"],
                 },
             ],
+        });
+        setHaveData(true);
+    }, [marathon]);
+
+    useEffect(() => {
+        SetChartOptions({
+            responsive: true,
+            plugins: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text:
+                        "Athlete Distribution --- " +
+                        marathon.num_athletes +
+                        " Total Athletes",
+                },
+            },
         });
         setHaveData(true);
     }, [marathon]);
@@ -32,7 +50,7 @@ export const BarMaleFemaleParticipants = ({ marathon }) => {
     }
     return (
         <div>
-            <Bar data={chartData} />
+            <Bar options={chartOptions} data={chartData} />
         </div>
     );
 };
