fa95e4ef45a3588d8ff360a3165effdabda45d89
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-12-20 20:54:16 -0600

    result model updates

--- src/backend/models/result.py
+++ src/backend/models/result.py
@@ -1,4 +1,7 @@
 from src.backend import db
+from marshmallow import Schema, fields
+from src.backend.models.athlete import AthleteSchema
+from src.backend.models.marathon import MarathonSchema
 
 
 class Result(db.Model):
@@ -8,7 +11,7 @@ class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     place_overall = db.Column(db.Integer, unique=False, nullable=False)
     place_gender = db.Column(db.Integer, nullable=False)
-    finish_time = db.Column(db.String(100), nullable=False)
+    finish_time = db.Column(db.Time, nullable=False)
     bib = db.Column(db.String(100), unique=False, nullable=False)
     age_group = db.Column(db.String(100), unique=False, nullable=False)
     marathon_event_id = db.Column(db.Integer, db.ForeignKey("MarathonEvents.id"))
@@ -19,3 +22,14 @@ class Result(db.Model):
 
     def __str__(self):
         return f"Result for {self.id}, Place Overall: {self.place_overall}, Place Gender: {self.place_gender}"
+
+
+
+class ResultSchema(Schema):
+    id = fields.Number()
+    place_overall = fields.Number()
+    place_gender = fields.Number()
+    finish_time = fields.Time()
+    age_group = fields.Str()
+    marathon_event = fields.Nested(MarathonSchema())
+    athlete = fields.Nested(AthleteSchema())
