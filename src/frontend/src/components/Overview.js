715348a2c55cc9d7282aa510f44aac9d67f0e02e
dankrzeminski32 <dankrzeminski32@gmail.com>
2022-12-20 20:55:10 -0600

    add  line finish times to default homepage

--- src/frontend/src/components/Overview.js
+++ src/frontend/src/components/Overview.js
@@ -3,6 +3,7 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import React from "react";
 import { BarMaleFemaleParticipants } from "./graphs/BarMaleFemaleParticipants";
+import { LineFinishTimes } from "./graphs/LineFinishTimes";
 
 function Overview({ marathon }) {
     return (
@@ -11,7 +12,9 @@ function Overview({ marathon }) {
                 <Col sm={6}>
                     <BarMaleFemaleParticipants marathon={marathon} />
                 </Col>
-                <Col sm={6}>sm=6</Col>
+                <Col sm={6}>
+                    <LineFinishTimes marathon={marathon} />
+                </Col>
             </Row>
             <Row className="h-50">
                 <Col sm={6}>sm=6</Col>
