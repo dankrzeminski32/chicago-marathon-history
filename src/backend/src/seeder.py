49d1c2c7819c18d6901786bd48e55f6c126ea3b7
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-02-21 20:51:37 -0600

    remove print statements

--- src/backend/src/seeder.py
+++ src/backend/src/seeder.py
@@ -6,13 +6,11 @@ class Seeder:
     def populate_table(self, data: list[Any]) -> None:
         """populates a table given a list"""
         for obj in data:
-            print(obj)
             db.session.add(obj)
         db.session.commit()
 
     def populate_athletes_and_results(self, data: list[tuple["Athlete", "Result"]]):
         """Used this way to get the PK value from athlete to be used in the result table"""
-        print(f"STARTING DB POPULATION, list size: {len(data)}")
         for list in data:
             for index, tup in enumerate(list):
                 db.session.add(tup[0])
